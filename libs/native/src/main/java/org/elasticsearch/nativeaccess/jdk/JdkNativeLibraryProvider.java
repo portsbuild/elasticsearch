657f0e8adcd869459aa80ceb0488a2d4dea4ed8b
Saro G. <saro@saromedia.com>
2025-01-16 10:20:00 -0500

    FreeBSD Native Access

--- libs/native/src/main/java/org/elasticsearch/nativeaccess/jdk/JdkNativeLibraryProvider.java
+++ libs/native/src/main/java/org/elasticsearch/nativeaccess/jdk/JdkNativeLibraryProvider.java
@@ -9,6 +9,7 @@
 
 package org.elasticsearch.nativeaccess.jdk;
 
+import org.elasticsearch.nativeaccess.lib.FreebsdCLibrary;
 import org.elasticsearch.nativeaccess.lib.JavaLibrary;
 import org.elasticsearch.nativeaccess.lib.Kernel32Library;
 import org.elasticsearch.nativeaccess.lib.LinuxCLibrary;
@@ -36,8 +37,10 @@ public class JdkNativeLibraryProvider extends NativeLibraryProvider {
                 VectorLibrary.class,
                 JdkVectorLibrary::new,
                 ParquetRsLibrary.class,
-                JdkParquetRsLibrary::new
-            )
+                JdkParquetRsLibrary::new,
+                FreebsdCLibrary.class,
+                JdkFreebsdCLibrary::new
+                )
         );
     }
 }
