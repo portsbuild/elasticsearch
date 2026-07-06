e399e89177e872aa1e6aff08fe2f8f88ff3b6bd1
Saro G. <saro@saromedia.com>
2026-07-06 13:25:38 -0400

    Preliminary support for Parquet-RS on FreeBSD

--- libs/native/src/main/java/org/elasticsearch/nativeaccess/PosixNativeAccess.java
+++ libs/native/src/main/java/org/elasticsearch/nativeaccess/PosixNativeAccess.java
@@ -242,7 +242,7 @@ public abstract class PosixNativeAccess extends AbstractNativeAccess {
     }
 
     static boolean isNativeRustLibSupported() {
-        return isMacOrLinuxAarch64() || isLinuxAmd64();
+        return isMacOrLinuxAarch64() || isLinuxAmd64() || isFreebsdAmd64();
     }
 
     /**
