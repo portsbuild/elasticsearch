5a7a24dcf05e112ff328d8bebf64392a97e8ee20
Saro G. <saro@saromedia.com>
2026-09-03 10:06:30 -0400

    [CI] Update workflow

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -4,7 +4,7 @@ mkdir ~/.gradle
 
 # "org.gradle.jvmargs="
 {
-  echo "runtime.java=/usr/local/openjdk25"
+  echo "runtime.java=/usr/local/openjdk26"
   echo "org.gradle.java.installations.paths=/usr/local/openjdk17,/usr/local/openjdk21,/usr/local/openjdk25,/usr/local/openjdk26"
   echo "org.gradle.java.installations.auto-download=false"
   echo "org.gradle.console=plain"
@@ -15,7 +15,7 @@ mkdir ~/.gradle
 } > ~/.gradle/gradle.properties
 
 {
-  echo "export RUNTIME_JAVA_HOME=/usr/local/openjdk25 ; export JAVA_TOOLCHAIN_HOME=/usr/local/openjdk25"
+  echo "export RUNTIME_JAVA_HOME=/usr/local/openjdk26 ; export JAVA_TOOLCHAIN_HOME=/usr/local/openjdk26"
   echo "export LOCAL_VEC_BINARY_OS=freebsd ; export LOCAL_ZSTD_BINARY=true ; export NATIVE_BUILD=local ;"
   echo 'export GRADLE_OPTS=${GRADLE_OPTS:+"$GRADLE_OPTS "}--enable-native-access=ALL-UNNAMED'
 } >> ~/.profile
